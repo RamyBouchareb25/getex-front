44818e467049aadc3222920a2ae9489100e6c7c6
Ramy Bouchareb <rami.bouchareb@etu.usthb.dz>
2025-07-03 00:14:34 +0100

    INIT

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "silly-jeana-dardzair-c1906402.koyeb.app",
+        port: "",
+        pathname: "/files/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
